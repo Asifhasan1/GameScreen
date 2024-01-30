afff0798d0614d79c3781a22866e72d1fbc09bf8
Asif <md.mushfikhasanasif@gmail.com>
2024-01-30 16:28:17 -0500

    Initial Commit

---
+++ Gamescreen/GamescreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamescreenApp.swift
+//  Gamescreen
+//
+//  Created by Asif on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct GamescreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
